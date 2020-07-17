bb4e1329b5b8d35833b021f78e131589a155006d
nag <nag@192.168.1.4>
2020-07-17 10:54:18 +0530

    Done

---
+++ src/com/nag4/set_ex4.java
@@ -0,0 +1,27 @@
+package com.nag4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class set_ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+				Set set = new HashSet();
+				set.add("one");
+				set.add("two");
+				set.add("three");
+				set.add("one");
+				set.add("four");
+				System.out.println(set);
+				
+				boolean removed = set.removeAll(set);
+				System.out.println(removed);
+				System.out.println(set);
+				
+			}
+
+		}
+
