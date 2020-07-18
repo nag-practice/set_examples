ba317ce2988211ef126bc3abe2b0a68ea2cd98c2
nag <nag@192.168.1.4>
2020-07-18 10:44:45 +0530

    Done

---
+++ src/com/nag11/retain_common_elements.java
@@ -0,0 +1,25 @@
+package com.nag11;
+
+import java.util.HashSet;
+
+public class retain_common_elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashSet<String> set = new HashSet<String>();
+		set.add("one");
+		set.add("two");
+		set.add("three");
+		set.add("one");
+		set.add("four");
+		System.out.println(set);
+		HashSet<String> set1 = new HashSet<String>();
+		boolean b = set1.retainAll(set);
+		
+		System.out.println(b);
+		
+		System.out.println(set1);
+	}
+
+}
